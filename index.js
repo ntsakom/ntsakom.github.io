90d4da6ff181759407c4b9e87d86e3dff0bcd300
Ntsako Maringa <ngmaringa@gmail.com>
2018-06-29 00:20:15 +0200

    Signed-off-by: Ntsako Maringa <ngmaringa@gmail.com>

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
+  navigator.serviceWorker.register('service-worker.js');
 }
+
+document.querySelector('#show').addEventListener('click', () => {
+  const iconUrl = document.querySelector('select').selectedOptions[0].value;
+  let imgElement = document.createElement('img');
+  imgElement.src = iconUrl;
+  document.querySelector('#container').appendChild(imgElement);
+});
