624ee0b60f4b113163c0771f42b70ec66d3d6b79
Ntsako Maringa <ngmaringa@gmail.com>
2018-06-29 03:08:28 +0200

    Signed-off-by: Ntsako Maringa <ngmaringa@gmail.com>

---
+++ currencyConverter.js
@@ -0,0 +1,34 @@
+//var https = require('https');
+
+function convertCurrency(amount, fromCurrency, toCurrency, cb) {
+  var apiKey = 'your-api-key-here';
+
+  fromCurrency = encodeURIComponent(fromCurrency);
+  toCurrency = encodeURIComponent(toCurrency);
+  var query = fromCurrency + '_' + toCurrency;
+
+  var url = 'https://free.currencyconverterapi.com/api/v5/convert?q='
+            + query + '&compact=ultra';
+
+  
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url , true);
+  xhr.send();
+
+  xhr.onreadystatechange = processRequest;
+
+  function processRequest(e) {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        var response = JSON.parse(xhr.responseText);
+        console.log(response);
+        alert(response.ip);
+    }
+}
+}
+
+
+
+//uncomment to test
+convertCurrency(10, 'USD', 'PHP', function(err, amount) {
+  console.log(amount);
+});
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+ console.log("Hello");
+
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('service-worker.js');
 }
